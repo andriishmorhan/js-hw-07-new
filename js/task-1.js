6b542d376854ca5502a28d0b278bbc6d9715d459
Andrew Shmorhan <140952286+andriishmorhan@users.noreply.github.com>
2025-01-09 13:33:00 +0100

    second task done

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,22 @@
-let list = document.querySelectorAll(".item");
+const list = document.querySelectorAll(".item");
+console.log("Number of categories: " + list.length);
 
-let listCount = list.length;
+// const listEl = list.forEach((element) => {
+//   console.log("Category: " + element.firstElementChild.textContent);
+//   console.log("Elements: ");
+// });
 
-console.log("Number of categories: " + listCount);
+// const nextEl = document.querySelectorAll("h2");
+// nextEl.forEach((element) => {
+//   const firstSubling = element.nextElementSibling;
+//   console.log(firstSubling.children.length);
+// });
+
+const listEl = document.querySelectorAll("h2");
+listEl.forEach((element) => {
+  console.log("Category: " + element.textContent);
+
+  const firstSibling = element.nextElementSibling;
+
+  console.log("Elements: " + firstSibling.children.length);
+});
