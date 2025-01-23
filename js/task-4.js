485d6cafd95dd0a78ca2113bb8bb0419e02a97bf
Andrew Shmorhan <140952286+andriishmorhan@users.noreply.github.com>
2025-01-23 15:16:08 +0100

    fourth task done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+
+  let login = "";
+  let password = "";
+
+  data.forEach((value, key) => {
+    if (key === "email") {
+      login = value.trim();
+    }
+    if ((key = "password")) {
+      password = value.trim();
+    }
+  });
+
+  if (login === "" || password == "") {
+    return alert("All form fields must be filled in");
+  }
+  console.log(`Login: ${login}, Password: ${password}`);
+  event.target.reset();
+});
