9003b8bc7e40f6e6d4078b4787a2df82ebe22d67
Andrew Shmorhan <140952286+andriishmorhan@users.noreply.github.com>
2025-02-11 22:18:41 +0100

    fifth task done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+
+const bodyEl = document.querySelector("body");
+
+const spanEl = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
+});
